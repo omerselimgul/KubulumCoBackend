b9cbcec4d704a04a687411f7de71c6206c0eca3b
Taylan <taylanalparslan@hotmail.com>
2022-11-25 00:31:42 +0300

    universite eklendi. update daha sonra eklenecek

--- Routes/index.js
+++ Routes/index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const login = require("./auth")
 const home = require("./home")
 const router = express.Router();
+const university = require("./university")
 
 router.use("/auth", login)
 router.use("/home", home)
+router.use("/university", university)
 
 module.exports = router
