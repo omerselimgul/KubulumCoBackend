fb4ff14cb0995ad2cc38bc1f80d0a8a4674fb1bf
Taylan <taylanalparslan@hotmail.com>
2022-11-27 19:10:26 +0300

    validation added.

--- Routes/club.js
+++ Routes/club.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const clubController = require("../controller/clubController")
+const validate = require("../middleware/validate/validate")
+const schema = require("../schemas/clubSchema")
 
-router.route("/").post(clubController.create)
-router.route("/").get(clubController.list)
-router.route("/:id").get(clubController.getById)
-router.route("/search/getByNameContains").get(clubController.getByClubNameContains)
-router.route("/:id").delete(clubController.deleteClub)
-router.route("/:id").patch(clubController.update)
+router.route("/").post(validate(schema.createSchema), clubController.create)
+router.route("/").get(validate(schema.getListSchema), clubController.list)
+router.route("/:id").get(validate(schema.getByIdSchema), clubController.getById)
+router.route("/search/getByNameContains").get(validate(schema.getByNameContainsSchema), clubController.getByClubNameContains)
+router.route("/:id").delete(validate(schema.deleteSchema), clubController.deleteClub)
+router.route("/:id").patch(validate(schema.updateSchema), clubController.update)
 
 module.exports = router
--- Routes/university.js
+++ Routes/university.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const universityController = require("../controller/universityContoller")
+const validate = require("../middleware/validate/validate")
+const schema = require("../schemas/universitySchema")
 
-router.route("/").post(universityController.create)
-router.route("/").get(universityController.list)
-router.route("/:id").get(universityController.getById)
-router.route("/search/getByName").get(universityController.getByNameContains)
-router.route("/:id").delete(universityController.deleteUniversity)
+router.route("/").post(validate(schema.createSchema), universityController.create)
+router.route("/").get(validate(schema.getListSchema), universityController.list)
+router.route("/:id").get(validate(schema.getByIdSchema), universityController.getById)
+router.route("/search/getByName").get(validate(schema.getByNameContainsSchema), universityController.getByNameContains)
+router.route("/:id").delete(validate(schema.deleteSchema), universityController.deleteUniversity)
 
 module.exports = router
