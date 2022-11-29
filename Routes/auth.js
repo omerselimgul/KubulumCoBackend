33abba0ae5249f4e0e2b0eb4e60065ebf572aea7
Selim <omer.gul@fimple.co.uk>
2022-11-30 02:34:24 +0300

    Repo

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,11 +1,16 @@
 const express = require("express")
-const { RegisterControllers, PostLoginController } = require("../controller/authController");
-const getAccessToRoute = require("../middleware/authorization/auth");
-
 const router = express.Router();
+const authController = require("../controller/authController");
+const validate = require("../middleware/validate/validate")
+const schema = require("../schemas/userSchema")
 
 
-router.post("/login", PostLoginController)
 
-router.post("/register",RegisterControllers)
+router.post("/login", validate(schema.postLoginSchema), authController.PostLoginController)
+router.post("/register", validate(schema.createSchema), authController.CreateUserControllers)
+
+// User Bura için güvenlik lazım gibi
+router.get("/:id", validate(schema.getByIdSchema), authController.getById)
+
+
 module.exports = router;
--- middleware/authorization/auth.js
+++ middleware/authorization/auth.js
@@ -2,24 +2,27 @@ const { CustomError } = require("../../helpers/error/CustomError")
 const jwt = require("jsonwebtoken")
 
 const getAccessToRoute = async (req, res, next) => {
-    const { token } = req.cookies
-    if (token) {
-        try {
-            const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
-            const { username, password, userid } = decodedToken
-            req.body.username = username
-            req.body.password = password
-            req.body.userid = userid
-            res.send({success: true})
-            next();
-        } catch (error) {
-            next(new CustomError(error, 400))
-            // res.status(401).send({ message: "Yetlisiz erisim", success: false })
-        }
-    } else {
-        res.send("Token yok")
-        // res.redirect("/api/login")
-    }
+    // const { token } = req.cookies
+    // if (token) {
+    //     try {
+    //         const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+    //         const { Username, Userpassword, UserId } = decodedToken
+    //         req.body.Username = Username
+    //         req.body.Userpassword = Userpassword
+    //         req.body.UserId = UserId
+
+    //         if (req.params.id === req.body.UserId) {
+    //             return next();
+    //         } else {
+    //             return next(new CustomError("Yetkisiz erisim", 400))
+    //         }
+    //     } catch (error) {
+    //         return next(new CustomError(error, 400))
+    //         // res.status(401).send({ message: "Yetkisiz erisim", success: false })
+    //     }
+    // } else {
+    //     return next(new CustomError("Yetkisiz erisim", 400))
+    // }
 }
 
 module.exports = getAccessToRoute
