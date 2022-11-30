19d67fdfda7c9d0f9c5130df3162ea918e1a4015
Selim <omer.gul@fimple.co.uk>
2022-11-30 03:33:25 +0300

    club admin

--- middleware/authorization/auth.js
+++ middleware/authorization/auth.js
@@ -2,27 +2,23 @@ const { CustomError } = require("../../helpers/error/CustomError")
 const jwt = require("jsonwebtoken")
 
 const getAccessToRoute = async (req, res, next) => {
-    // const { token } = req.cookies
-    // if (token) {
-    //     try {
-    //         const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
-    //         const { Username, Userpassword, UserId } = decodedToken
-    //         req.body.Username = Username
-    //         req.body.Userpassword = Userpassword
-    //         req.body.UserId = UserId
-
-    //         if (req.params.id === req.body.UserId) {
-    //             return next();
-    //         } else {
-    //             return next(new CustomError("Yetkisiz erisim", 400))
-    //         }
-    //     } catch (error) {
-    //         return next(new CustomError(error, 400))
-    //         // res.status(401).send({ message: "Yetkisiz erisim", success: false })
-    //     }
-    // } else {
-    //     return next(new CustomError("Yetkisiz erisim", 400))
-    // }
+    const { token } = req.cookies
+    if (token) {
+        try {
+            const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+            const { Username, Userpassword, UserId } = decodedToken
+            req.body.Username = Username
+            req.body.Userpassword = Userpassword
+            req.body.UserId = UserId
+            return next();
+        } catch (error) {
+            return next(new CustomError(error, 400))
+        }
+    } else {
+        return next(new CustomError("Giriş yapınız", 400))
+    }
 }
 
-module.exports = getAccessToRoute
+module.exports = {
+    getAccessToRoute
+}
