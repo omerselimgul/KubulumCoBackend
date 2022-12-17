a887883c2c186789e6189d669fb2d2dcdc33fd53
Selim <omer.gul@fimple.co.uk>
2022-12-18 01:21:22 +0300

    universite-fix

--- controller/userController.js
+++ controller/userController.js
@@ -37,10 +37,10 @@ const EditUser = async (req, res, next) => {
     try {
 
         const beforeUserData = await userRepository.getById(req.body.UserId)
-
+        console.log(beforeUserData)
         req.body.Username = req.body.Username ?? beforeUserData.Username
         req.body.Email = req.body.Email ?? beforeUserData.Email
-        req.body.Universite = req.body.University ?? beforeUserData.UniversityId
+        req.body.Universite = req.body.Universite ?? beforeUserData.UniversityId
         req.body.Birthdate = req.body.Birthdate ?? beforeUserData.Birthdate
         req.body.Cinsiyet = req.body.Cinsiyet ?? beforeUserData.Cinsiyet
         req.body.Bolum = req.body.Bolum ?? beforeUserData.Bolum
